9c82781db70ea13cf176d19df751ee57f95ea287
Labores olis <122188954+laboressolis@users.noreply.github.com>
2025-02-21 18:50:41 +0530

    end_screen.py: fix end screen

--- src/game_states/end_screen.py
+++ src/game_states/end_screen.py
@@ -9,25 +9,23 @@ def end(screen, clock, result):
                 if event.key == pygame.K_ESCAPE:
                     return "main_menu"
         screen.fill((0, 0, 0))
+        font = pygame.font.Font("../tictactoe-pygame/src/assets/fonts/PressStart2P.ttf", 35)
         if result == 'draw':
-            font = pygame.font.Font("../tictactoe-pygame/src/assets/fonts/PressStart2P.ttf", 20)
             title_text = font.render("It's a draw!", True, (255, 255, 255))
-            title_rect = title_text.get_rect(center=(300, 50))
+            title_rect = title_text.get_rect(center=(300, 300))
             screen.blit(title_text, title_rect)
         else:
             if result == 'X':
-                font = pygame.font.Font("../tictactoe-pygame/src/assets/fonts/PressStart2P.ttf", 20)
-                title_text = font.render("Player 1 wins!", True, (255, 255, 255))
-                title_rect = title_text.get_rect(center=(300, 50))
+                title_text = font.render("Player X wins!", True, (255, 255, 255))
+                title_rect = title_text.get_rect(center=(300, 300))
                 screen.blit(title_text, title_rect)
             else:
-                font = pygame.font.Font("../tictactoe-pygame/src/assets/fonts/PressStart2P.ttf", 20)
-                title_text = font.render("Player 2 wins!", True, (255, 255, 255))
-                title_rect = title_text.get_rect(center=(300, 50))
+                title_text = font.render("Player O wins!", True, (255, 255, 255))
+                title_rect = title_text.get_rect(center=(300, 300))
                 screen.blit(title_text, title_rect)
         font = pygame.font.Font("../tictactoe-pygame/src/assets/fonts/PressStart2P.ttf", 20)
         title_text = font.render("Press 'ESC' to go back!", True, (255, 255, 255))
-        title_rect = title_text.get_rect(center=(300, 100))
+        title_rect = title_text.get_rect(center=(300, 500))
         screen.blit(title_text, title_rect)
         pygame.display.update()
         clock.tick(60)
